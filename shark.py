e6b336be761adaac7810d80b329c9f5fd6c0eb8b
Sddilora <sumeyyedilaradogan@gmail.com>
2023-04-07 13:24:50 +0300

    api initialized

---
+++ shark.py
@@ -0,0 +1,16 @@
+# GET /api/v1/maps/3413/pois.geojson/?h=10
+import json
+import requests
+import googlemaps
+
+data = None
+with open("shark_data.json", "r") as f:
+    data = json.load(f)
+# print (data)
+
+for shark in data["features"]:
+    # print(shark["properties"])
+    print("Name: " + shark["properties"]["name"])
+    print("Stage of Life: " + shark["properties"]["stage_of_life"])
+    print("Gender: " + shark["properties"]["gender"])
+    print("")
